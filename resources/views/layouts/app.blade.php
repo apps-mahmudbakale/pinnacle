69054bb7a126c289b470cc7cc7e58e62a61d00e5
Mahmud Bakale <app.mahmud.bakale@gmail.com>
2025-09-09 19:42:58 +0100

    Wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,25 +7,25 @@
 	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	<title>Home Login</title>
 	<link rel="shortcut icon" type="image/x-icon" href="{{URL::to('assets/img/favicon.png')}}">
-	<link rel="stylesheet" href="{{URL::to('assets/css/bootstrap.min.css')}}">
-	<link rel="stylesheet" href="{{URL::to('assets/plugins/fontawesome/css/fontawesome.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{URL::to('assets/plugins/fontawesome/css/all.min.css')}}">
-	<link rel="stylesheet" href="{{URL::to('assets/css/feathericon.min.css')}}">
-	<link rel="stylesheet" href="{{URL::to('assets/plugins/morris/morris.css')}}">
-	<link rel="stylesheet" href="{{URL::to('assets/css/style.css')}}"> </head>
+	<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/plugins/fontawesome/css/fontawesome.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('assets/plugins/fontawesome/css/all.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/feathericon.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/plugins/morris/morris.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/style.css')}}"> </head>
 	{{-- message toastr --}}
-	<link rel="stylesheet" href="{{ URL::to('assets/css/toastr.min.css') }}">
-	<script src="{{ URL::to('assets/js/toastr_jquery.min.js') }}"></script>
-	<script src="{{ URL::to('assets/js/toastr.min.js') }}"></script>
+	<link rel="stylesheet" href="{{ asset('assets/css/toastr.min.css') }}">
+	<script src="{{ asset('assets/js/toastr_jquery.min.js') }}"></script>
+	<script src="{{ asset('assets/js/toastr.min.js') }}"></script>
 
 <body>
 	@yield('content')
 
-	<script src="{{URL::to('assets/js/jquery-3.5.1.min.js')}}"></script>
-	<script src="{{URL::to('assets/js/popper.min.js')}}"></script>
-	<script src="{{URL::to('assets/js/bootstrap.min.js')}}"></script>
-	<script src="{{URL::to('assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
-	<script src="{{URL::to('assets/js/script.js')}}"></script>
+	<script src="{{asset('assets/js/jquery-3.5.1.min.js')}}"></script>
+	<script src="{{asset('assets/js/popper.min.js')}}"></script>
+	<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+	<script src="{{asset('assets/js/script.js')}}"></script>
 </body>
 
 </html>
